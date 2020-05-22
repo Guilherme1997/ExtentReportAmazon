bc82d57d82db7301b7feec72c0fc0c1130e5fe25
Guilherme <guilhermego1997@gmail.com>
2020-05-22 15:28:37 -0300

    Refatoração e inclusão de features

--- ExtentReportAmazon/Common/Hooks.cs
+++ ExtentReportAmazon/Common/Hooks.cs
@@ -17,7 +17,7 @@ namespace ExtentReportAmazon.Common
 		private static readonly string PathReport = $"{AppDomain.CurrentDomain.BaseDirectory}ExtentReportAmazon.html";
 
 		[BeforeTestRun]
-		public static void ConfigureReport()
+		public static void ConfigurarRelatorio()
 		{
 			var reporter = new ExtentHtmlReporter(PathReport);
 			_extent = new ExtentReports();
@@ -25,38 +25,39 @@ namespace ExtentReportAmazon.Common
 		}
 
 		[BeforeFeature]
-		public static void CreateFeature()
+		public static void CriarFeature()
 		{
 			_feature = _extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
 		}
 
 		[BeforeScenario]
-		public static void CreateScenario()
+		public static void CriarCenario()
 		{
 			_scenario = _feature.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
 		}
 
 		[AfterStep]
-		public static void InsertReportingSteps()
+		public static void InserirRelatorios()
 		{
 			switch (ScenarioStepContext.Current.StepInfo.StepDefinitionType)
 			{
 				case StepDefinitionType.Given:
-					_scenario.StepDefinitionGiven();
+					_scenario.DefinicaoEtapaFornecida();
 					break;
 
 				case StepDefinitionType.Then:
-					_scenario.StepDefinitionThen();
+					_scenario.DefinicaoEtapaFornecida();
 					break;
 
 				case StepDefinitionType.When:
-					_scenario.StepDefinitionWhen();
+					_scenario.DefinicaoEtapaFornecida();
 					break;
 			}
 		}
 
 		[AfterTestRun]
-		public static void FlushExtent()
+		public static void ExecutarProcessoRelatorio
+()
 		{
 			_extent.Flush();
 			System.Diagnostics.Process.Start(PathReport);
---
+++ ExtentReportAmazon/ExtensionMethods/ScenarioExtensionMethod.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Bindings;
+
+namespace ExtentReportAmazon.ExtensionMethods
+{
+	public static class ScenarioExtensionMethod
+	{
+		private static ExtentTest CriarCenario(ExtentTest extent, StepDefinitionType stepDefinitionType)
+		{
+			var scenarioStepContext = ScenarioStepContext.Current.StepInfo.Text;
+
+			switch (stepDefinitionType)
+			{
+				case StepDefinitionType.Given:
+					return extent.CreateNode<Given>(scenarioStepContext);
+
+				case StepDefinitionType.Then:
+					return extent.CreateNode<Then>(scenarioStepContext);
+
+				case StepDefinitionType.When:
+					return extent.CreateNode<When>(scenarioStepContext);
+				default:
+					throw new ArgumentOutOfRangeException(nameof(stepDefinitionType), stepDefinitionType, null);
+			}
+		}
+
+		private static void CriarCenaroDeErro(ExtentTest extent, StepDefinitionType stepDefinitionType)
+		{
+			var error = ScenarioContext.Current.TestError;
+
+			if (error.InnerException == null)
+			{
+				CriarCenario(extent, stepDefinitionType).Error(error.Message);
+			}
+			else
+			{
+				CriarCenario(extent, stepDefinitionType).Fail(error.InnerException);
+			}
+		}
+
+		public static void DefinicaoEtapaFornecida(this ExtentTest extent)
+		{
+			if (ScenarioContext.Current.TestError == null)
+				CriarCenario(extent, StepDefinitionType.Given);
+			else
+				CriarCenaroDeErro(extent, StepDefinitionType.Given);
+		}
+
+		public static void EtapaDefinicao(this ExtentTest extent)
+		{
+			if (ScenarioContext.Current.TestError == null)
+				CriarCenario(extent, StepDefinitionType.When);
+			else
+				CriarCenaroDeErro(extent, StepDefinitionType.When);
+		}
+
+		public static void DefinicaoDaEtapa (this ExtentTest extent)
+		{
+			if (ScenarioContext.Current.TestError == null)
+				CriarCenario(extent, StepDefinitionType.Then);
+			else
+				CriarCenaroDeErro(extent, StepDefinitionType.Then);
+		}
+	}
+}
