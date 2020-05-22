b69820b23cadbfe39f4d338f37bbd4d9a5f3fc8d
Guilherme <guilhermego1997@gmail.com>
2020-05-22 14:45:16 -0300

    Alterações features

---
+++ ExtentReportAmazon/Steps/AmazonBuyDellXPS.cs
@@ -0,0 +1,55 @@
+﻿using BoDi;
+using ExtentReportAmazon.Common;
+using ExtentReportAmazon.Pages;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace ExtentReportAmazon.Steps
+{
+	[Binding]
+	public sealed class AmazonBuyDellXPS : Bootstrapper
+	{
+		private readonly AmazonPage _amazonPage;
+
+		public AmazonBuyDellXPS(IObjectContainer objectContainer) : base(objectContainer)
+		{
+			_amazonPage = new AmazonPage(this.Driver);
+		}
+
+		[Given(@"I open Amazon website")]
+		public void DadoQueAbrirSiteAmazon()
+		{
+			_amazonPage.AbraSiteAmazon();
+		}
+
+		[Given(@"I search by Soap Dove")]
+		public void DadoQueEuPesquiseiPorProduto()
+		{
+			_amazonPage.ProcurarPor("dell xps");
+		}
+
+		[Given(@"I press enter key for submit")]
+		public void DadoQueEuPressioneATeclaEnteParaEnviar()
+		{
+			_amazonPage.PressioneTeclaEnterParaEnviar();
+		}
+
+		[Given(@"I choose the first item of list")]
+		public void DadoQueEuEscolhiOPrimeiroItemDaLista()
+		{
+			_amazonPage.EscolhaPrimeiroItemLista();
+		}
+
+		[When(@"I click in Add to Cart button")]
+		public void QuandoClicoEmAdicionarAoCarrinho()
+		{
+			_amazonPage.CliqueAdicionarCarrinho();
+		}
+
+		[Then(@"Must have one item in cart")]
+		public void FinalizarCompra()
+		{
+			_amazonPage.FinalizarCompra();
+		}
+	}
+}
